c7e32f1b232a200955cf639e26e0588bf5d00293
Ariel M <ariel.macapas@gmail.com>
2025-09-15 00:26:30 +0800

    setup github pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/VCL",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
